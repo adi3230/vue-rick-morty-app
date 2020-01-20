e01783445e00fb25f3547cc8c9a0edda6cb0e07f
Aditya Joshi <adi3230@gmail.com>
2020-01-20 23:06:46 +0100

    add some margins

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,24 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/ricky-morty">Ricky & Morty</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+body {
+  margin: 0;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background-color: black;
 }
 
 #nav {
@@ -22,7 +27,7 @@
 
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: grey;
 
     &.router-link-exact-active {
       color: #42b983;
