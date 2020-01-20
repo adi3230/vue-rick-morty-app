4b1fb59f7d52d1dc7da8c2d90fbf56690beffe49
Aditya Joshi <adi3230@gmail.com>
2020-01-20 23:08:50 +0100

    fetch list through API

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,30 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios';
+
+axios.defaults.baseURL = "https://rickandmortyapi.com/api";
 
 Vue.use(Vuex)
 
+const SET_CHARACTER_LIST = 'SET_CHARACTER_LIST';
+
 export default new Vuex.Store({
   state: {
+    characters: [],
   },
   mutations: {
+    SET_CHARACTER_LIST(state, characterList) {
+      state.characters = characterList;
+    },
   },
   actions: {
+    async fetchCharacterList({ commit }) {
+      const response = await axios.get('/character');
+      console.log('result', response.data);
+      if(Array.isArray(response.data.results)) {
+        commit(SET_CHARACTER_LIST, response.data.results);
+      }
+    },
   },
   modules: {
   }
