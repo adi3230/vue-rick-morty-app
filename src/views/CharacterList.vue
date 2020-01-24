4e998b1f984bb4461692bcea453fa967c4ca86da
Aditya Joshi <adi3230@gmail.com>
2020-01-24 23:07:22 +0100

    flip effect

--- src/views/CharacterList.vue
+++ src/views/CharacterList.vue
@@ -3,10 +3,25 @@
         <div class="character-list__item" 
                 v-for="character in characterList" 
                 :key="character.id">
-                <img :src="character.image" class="character-list__item-image" alt="Character Image"/>
-            <p>Name: {{character.name}}</p>
-            <p>Status: {{character.status}}</p>
-            <p>Species: {{character.species}}</p>
+                <div class="flip-card">
+                    <div class="flip-card-inner">
+                        <div class="flip-card-front">
+                            <img :src="character.image"
+                            class="character-list__item-image"
+                            alt="Avatar">
+                        </div>
+                        <div class="flip-card-back">
+                            <h1>{{character.name}}</h1>
+                            <ul class="character-list__item-info">
+                                <li>{{character.species}}</li>
+                                <li>{{character.status}}</li>
+                                <li>{{character.gender}}</li>
+                                <li>{{character.location.name}}</li>
+                                <li>{{character.species}}</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
         </div>
     </div>
 </template>
@@ -34,13 +49,53 @@ export default {
         margin: 1rem;
     }
     &__item {
-        border-radius: 20px;
-        background-color: white;
+        background-color: black;
         &-image {
             width: 100%;
-            border-top-left-radius: 20px;
-            border-top-right-radius: 20px;
+            border-radius: 20px;
+        }
+        &-info {
+            text-align: left;
         }
     }
+}
+.flip-card {
+  background-color: transparent;
+  width: 300px;
+  height: 300px;
+  perspective: 1000px;
+}
+
+.flip-card-inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: transform 0.6s;
+  transform-style: preserve-3d;
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+}
+
+.flip-card:hover .flip-card-inner {
+  transform: rotateY(180deg);
+}
+
+.flip-card-front, .flip-card-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  backface-visibility: hidden;
+  border-radius: 20px;
+}
+
+.flip-card-front {
+  background-color: #bbb;
+  color: black;
+}
+
+.flip-card-back {
+  background-color: white;
+  // color: white;
+  transform: rotateY(180deg);
 }
 </style>
